29b2d30364c88affe01ab635ead1a6945843b5f9
Jacky <jacky_cjs@163.com>
2017-04-06 18:04:15 +0800

    update

--- server/action/react-demo1-getMessageList.js
+++
@@ -1,7 +0,0 @@
-var getMessageList = require('../../test/react-demo1-MessageList.js');
-
-exports.excute = function(req, res) {
-    getMessageList.getMessageList(function(data) {
-        res.send(data);
-    })
-}
